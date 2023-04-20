dbf4b5f74ecac469c42d29192ade86c846478b03
Josh Gross <joshmgross@github.com>
2023-04-20 18:15:51 -0400

    Create a separate type for tokens with range

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq, Clone)]
-pub enum TokenType {
+pub enum Token {
     LeftParen,
     RightParen,
     LeftBracket,
@@ -40,9 +40,15 @@ pub struct Range {
 }
 
 #[derive(Debug)]
-pub struct Token {
-    pub token_type: TokenType,
-    pub range: Range, // TODO: Can this be included in the TokenType enum, removing the need for a separate Token struct?
+pub struct TokenWithRange {
+    pub token: Token,
+    pub range: Range,
+}
+
+impl TokenWithRange {
+    pub fn new(token: Token, range: Range) -> Self {
+        Self { token, range }
+    }
 }
 
 pub struct Lexer {
@@ -52,7 +58,7 @@ pub struct Lexer {
     offset: usize,           // Current offset in input.
     line: usize,             // Current line number.
     last_line_offset: usize, // Offset of last line break.
-    tokens: Vec<Token>,
+    tokens: Vec<TokenWithRange>,
 }
 
 impl Lexer {
@@ -68,41 +74,41 @@ impl Lexer {
         }
     }
 
-    pub fn lex(mut self) -> Vec<Token> {
+    pub fn lex(mut self) -> Vec<TokenWithRange> {
         // TODO: Validate max expression length.
         while !self.at_end() {
             self.start = self.offset;
             match self.next() {
-                '(' => self.add_token(TokenType::LeftParen),
-                ')' => self.add_token(TokenType::RightParen),
-                '[' => self.add_token(TokenType::LeftBracket),
-                ']' => self.add_token(TokenType::RightBracket),
-                ',' => self.add_token(TokenType::Comma),
+                '(' => self.add_token(Token::LeftParen),
+                ')' => self.add_token(Token::RightParen),
+                '[' => self.add_token(Token::LeftBracket),
+                ']' => self.add_token(Token::RightBracket),
+                ',' => self.add_token(Token::Comma),
                 '.' => {
                     let previous = self.previous();
                     match previous {
-                        TokenType::Identifier(_)
-                        | TokenType::RightBracket
-                        | TokenType::LeftBracket
-                        | TokenType::Star => {
+                        Token::Identifier(_)
+                        | Token::RightBracket
+                        | Token::LeftBracket
+                        | Token::Star => {
                             self.consume_number();
                         }
                         _ => {
-                            self.add_token(TokenType::Dot);
+                            self.add_token(Token::Dot);
                         }
                     }
                 }
                 '-' | '+' => self.consume_number(),
                 '!' => {
                     if self.match_char('=') {
-                        self.add_token(TokenType::BangEqual);
+                        self.add_token(Token::BangEqual);
                     } else {
-                        self.add_token(TokenType::Bang);
+                        self.add_token(Token::Bang);
                     }
                 }
                 '=' => {
                     if self.match_char('=') {
-                        self.add_token(TokenType::EqualEqual);
+                        self.add_token(Token::EqualEqual);
                     } else {
                         // Illegal; continue reading until we hit a boundary character and return an error
                         self.consume_identifier();
@@ -110,21 +116,21 @@ impl Lexer {
                 }
                 '<' => {
                     if self.match_char('=') {
-                        self.add_token(TokenType::LessEqual);
+                        self.add_token(Token::LessEqual);
                     } else {
-                        self.add_token(TokenType::Less);
+                        self.add_token(Token::Less);
                     }
                 }
                 '>' => {
                     if self.match_char('=') {
-                        self.add_token(TokenType::GreaterEqual);
+                        self.add_token(Token::GreaterEqual);
                     } else {
-                        self.add_token(TokenType::Greater);
+                        self.add_token(Token::Greater);
                     }
                 }
                 '&' => {
                     if self.match_char('&') {
-                        self.add_token(TokenType::And);
+                        self.add_token(Token::And);
                     } else {
                         // Illegal; continue reading until we hit a boundary character and return an error
                         self.consume_identifier();
@@ -132,13 +138,13 @@ impl Lexer {
                 }
                 '|' => {
                     if self.match_char('|') {
-                        self.add_token(TokenType::Or);
+                        self.add_token(Token::Or);
                     } else {
                         // Illegal; continue reading until we hit a boundary character and return an error
                         self.consume_identifier();
                     }
                 }
-                '*' => self.add_token(TokenType::Star),
+                '*' => self.add_token(Token::Star),
                 ' ' | '\r' | '\t' => {
                     // Ignore whitespace.
                 }
@@ -152,10 +158,8 @@ impl Lexer {
             }
         }
 
-        self.tokens.push(Token {
-            token_type: TokenType::Eof,
-            range: self.range(),
-        });
+        self.tokens
+            .push(TokenWithRange::new(Token::Eof, self.range()));
 
         self.tokens
     }
@@ -191,11 +195,11 @@ impl Lexer {
         self.input[self.offset + 1]
     }
 
-    fn previous(&self) -> TokenType {
+    fn previous(&self) -> Token {
         if self.tokens.len() == 0 {
-            TokenType::Eof
+            Token::Eof
         } else {
-            self.tokens[self.tokens.len() - 1].token_type.clone()
+            self.tokens[self.tokens.len() - 1].token.clone()
         }
     }
 
@@ -203,12 +207,8 @@ impl Lexer {
         self.offset >= self.input.len()
     }
 
-    fn add_token(&mut self, token_type: TokenType) {
-        let token = Token {
-            token_type,
-            range: self.range(),
-        };
-        self.tokens.push(token);
+    fn add_token(&mut self, token: Token) {
+        self.tokens.push(TokenWithRange::new(token, self.range()));
     }
 
     fn pos(&self) -> Pos {
@@ -239,7 +239,7 @@ impl Lexer {
 
         let lexeme = self.input_string[self.start..self.offset].to_string();
         let f = lexeme.parse::<f64>().unwrap();
-        self.add_token(TokenType::Number(f));
+        self.add_token(Token::Number(f));
     }
 
     fn consume_string(&mut self) {
@@ -267,7 +267,7 @@ impl Lexer {
             .to_string()
             .replace("''", "'");
         // Replace every '' with '
-        self.add_token(TokenType::String(lexeme));
+        self.add_token(Token::String(lexeme));
     }
 
     fn consume_identifier(&mut self) {
@@ -280,14 +280,14 @@ impl Lexer {
             // TODO: Error here
         }
 
-        if self.previous() != TokenType::Dot {
+        if self.previous() != Token::Dot {
             let token = match lexeme {
-                "true" => TokenType::True,
-                "false" => TokenType::False,
-                "null" => TokenType::Null,
-                "NaN" => TokenType::Number(f64::NAN),
-                "Infinity" => TokenType::Number(f64::INFINITY),
-                _ => TokenType::Identifier(lexeme.to_string()),
+                "true" => Token::True,
+                "false" => Token::False,
+                "null" => Token::Null,
+                "NaN" => Token::Number(f64::NAN),
+                "Infinity" => Token::Number(f64::INFINITY),
+                _ => Token::Identifier(lexeme.to_string()),
             };
             self.add_token(token)
         }
@@ -335,74 +335,70 @@ mod test {
         };
     }
 
-    fn assert_tokens(input: &str, expected: &[TokenType]) {
+    fn assert_tokens(input: &str, expected: &[Token]) {
         let mut e = expected.to_vec();
-        e.push(TokenType::Eof);
+        e.push(Token::Eof);
 
         let actual = Lexer::new(String::from(input))
             .lex()
             .iter()
-            .map(|t| t.token_type.clone())
-            .collect::<Vec<TokenType>>();
+            .map(|t| t.token.clone())
+            .collect::<Vec<Token>>();
         assert_eq!(e, actual);
     }
 
     test!(test_empty, "", &[]);
-    test!(test_number, "123", &[TokenType::Number(123.0)]);
-    test!(test_number_with_dot, "123.", &[TokenType::Number(123.0)]);
+    test!(test_number, "123", &[Token::Number(123.0)]);
+    test!(test_number_with_dot, "123.", &[Token::Number(123.0)]);
 
-    test!(test_less, "<", &[TokenType::Less]);
-    test!(test_less_equal, "<=", &[TokenType::LessEqual]);
-    test!(test_greater, ">", &[TokenType::Greater]);
-    test!(test_greater_equal, ">=", &[TokenType::GreaterEqual]);
-    test!(test_equal, "==", &[TokenType::EqualEqual]);
-    test!(test_not_equal, "!=", &[TokenType::BangEqual]);
-    test!(test_and, "&&", &[TokenType::And]);
-    test!(test_or, "||", &[TokenType::Or]);
+    test!(test_less, "<", &[Token::Less]);
+    test!(test_less_equal, "<=", &[Token::LessEqual]);
+    test!(test_greater, ">", &[Token::Greater]);
+    test!(test_greater_equal, ">=", &[Token::GreaterEqual]);
+    test!(test_equal, "==", &[Token::EqualEqual]);
+    test!(test_not_equal, "!=", &[Token::BangEqual]);
+    test!(test_and, "&&", &[Token::And]);
+    test!(test_or, "||", &[Token::Or]);
 
     test!(
         test_string,
         "'hello'",
-        &[TokenType::String(String::from("hello"))]
+        &[Token::String(String::from("hello"))]
     );
 
     test!(
         test_string_with_single_quote,
         "'It''s a string'",
-        &[TokenType::String(String::from("It's a string"))]
+        &[Token::String(String::from("It's a string"))]
     );
 
     test!(
         test_array,
         "[1, 2, 3]",
         &[
-            TokenType::LeftBracket,
-            TokenType::Number(1.0),
-            TokenType::Comma,
-            TokenType::Number(2.0),
-            TokenType::Comma,
-            TokenType::Number(3.0),
-            TokenType::RightBracket
+            Token::LeftBracket,
+            Token::Number(1.0),
+            Token::Comma,
+            Token::Number(2.0),
+            Token::Comma,
+            Token::Number(3.0),
+            Token::RightBracket
         ]
     );
 
     test!(
         test_expression,
         "1 == 2",
-        &[
-            TokenType::Number(1.0),
-            TokenType::EqualEqual,
-            TokenType::Number(2.0)
-        ]
+        &[Token::Number(1.0), Token::EqualEqual, Token::Number(2.0)]
     );
 
     test!(
         test_identifier,
         "foo",
-        &[TokenType::Identifier(String::from("foo"))]
+        &[Token::Identifier(String::from("foo"))]
     );
 
-    test!(test_true, "true", &[TokenType::True]);
-    test!(test_false, "false", &[TokenType::False]);
-    test!(test_null, "null", &[TokenType::Null]);
+    test!(test_true, "true", &[Token::True]);
+    test!(test_false, "false", &[Token::False]);
+    test!(test_null, "null", &[Token::Null]);
 }
